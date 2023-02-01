3d42e2d31ba47227959d5ebc98c1fb6b94380f48
jpt <jpt@jpt.com>
2023-02-01 23:05:17 +0545

    updated the app
    removed repeated lines of codes

--- main.py
+++ main.py
@@ -32,35 +32,35 @@ def create_five_tabs(nb):
 
 # ---------------------------- BACKEND?? --------------------------------- #
 
-def add_hw_button(hw_tab):
-    global notebook, due_entry, subject_entry, description_entry
+def add_hw_button(tab_hw):
+    global notebook, hw_due_entry, hw_subject_entry, hw_content_entry
     notebook.tab(4, state='normal')
     notebook.select(4)
     
     # 1st row
-    Label(hw_tab, text="Enter Subject", font=FONT, pady=10, padx=20).grid(row=1, column=1)
-    subject_entry = Entry(hw_tab, width=40, font=FONT)
-    subject_entry.grid(row=1, column=2)
+    Label(tab_hw, text="Enter Subject", font=FONT, pady=10, padx=20).grid(row=1, column=1)
+    hw_subject_entry = Entry(tab_hw, width=40, font=FONT)
+    hw_subject_entry.grid(row=1, column=2)
     
     # 2nd row
-    Label(hw_tab, text="Enter Description", font=FONT, pady=10, padx=20).grid(row=2, column=1)
-    description_entry = Entry(hw_tab, width=40, font=FONT)
-    description_entry.grid(row=2, column=2)
+    Label(tab_hw, text="Enter Description", font=FONT, pady=10, padx=20).grid(row=2, column=1)
+    hw_content_entry = Entry(tab_hw, width=40, font=FONT)
+    hw_content_entry.grid(row=2, column=2)
     
     # 3rd row
-    Label(hw_tab, text="Enter Due date", font=FONT, pady=10, padx=20).grid(row=3, column=1)
-    due_entry = Entry(hw_tab, width=40, font=FONT)
-    due_entry.grid(row=3, column=2)
+    Label(tab_hw, text="Enter Due date", font=FONT, pady=10, padx=20).grid(row=3, column=1)
+    hw_due_entry = Entry(tab_hw, width=40, font=FONT)
+    hw_due_entry.grid(row=3, column=2)
     
     # add button
-    Button(hw_tab, text="  Save  ", font=FONT, command=save_hw).grid(row=9, column=5)
+    Button(tab_hw, text="  Save  ", font=FONT, command=save_hw).grid(row=9, column=5)
 
 
 def show_homework():
     global assignments_tab
     try:
-        with open("data.json") as file:
-            hw = json.load(file)['homework']
+        with open("data.json") as read_file:
+            hw = json.load(read_file)['homework']
     except FileNotFoundError:
         messagebox.showerror(title="File not found!", message="No data file has been found!")
     else:
@@ -76,81 +76,132 @@ def show_homework():
 
 
 def save_hw():
-    global due_entry, subject_entry, description_entry, sub_id
-    sub_id += 1
-    with open("id.txt", mode='w') as id_file:
-        id_file.write(f"{sub_id}")
+    global hw_due_entry, hw_subject_entry, hw_content_entry, hw_id, whole_json_data
+    hw_id += 1
+    with open("id.txt", mode='w') as write_file:
+        write_file.write(f"{hw_id}")
     
-    subject = subject_entry.get()
-    due = due_entry.get()
-    description = description_entry.get()
+    subject = hw_subject_entry.get()
+    due = hw_due_entry.get()
+    description = hw_content_entry.get()
     
-    new_data = {
+    new_homework = {
         f"{subject}": {
             "description": f"{description}",
             "due": f"{due}",
             "completed": False,
-            "id": sub_id
+            "id": hw_id
         }
     }
-    
-    if len(subject) == 0 or len(description) == 0:
+    field_is_empty = len(subject) == 0 or len(description) == 0
+    if field_is_empty:
         messagebox.showerror(title="EmptyFieldError!", message="Please don't leave any field empty.")
     else:
-        try:
-            with open("data.json", mode='r') as file:
-                Whole_json_data = json.load(file)
+        whole_json_data['homework'].update(new_homework)
+        with open("data.json", mode='w') as write_file:
+            json.dump(whole_json_data, write_file, indent=4)
+    
+        messagebox.showinfo(title='Homework Added', message="Homework has been added! ")
+        messagebox.Message(notebook)
+        hw_due_entry.delete(0, END)
+        hw_content_entry.delete(0, END)
+        hw_subject_entry.delete(0, END)
         
-        except FileNotFoundError:
-            with open('data.json', mode='w') as file:
-                file_structure = {
-                    "homework": new_data,
-                    "todo": {},
-                    "notes": {},
-                    "completed_hw": {}
-                }
-                json.dump(file_structure, file, indent=4)
-        
-        else:
-            Whole_json_data['homework'].update(new_data)
-            with open("data.json", mode='w') as file:
-                json.dump(Whole_json_data, file, indent=4)
-        
-        finally:
-            messagebox.showinfo(title='Homework Added', message="Homework has been added! ")
-            messagebox.Message(notebook)
-            due_entry.delete(0, END)
-            description_entry.delete(0, END)
-            subject_entry.delete(0, END)
-            
-            notebook.tab(4, state='hidden')
-            notebook.select(1)
-            show_homework()
+        notebook.tab(4, state='hidden')
+        notebook.select(1)
+        show_homework()
 
 
 def save_todo():
-    create_todo_button.configure(state='normal')
+    global todo_content_entry, todo_title_entry, todo_tab, todo_create_button, todo_content_label, todo_title_label, todo_id, whole_json_data
+    todo_id += 1
+    todo_create_button.configure(state='normal')
+    new_todo = Todo()
+    todo_create_button.configure(state='normal')
 
-
+    with open("todo_id.txt", mode='w') as todo_id_file:
+        new_todo.id = todo_id_file.write(f'{todo_id}')
+        
+    new_todo.title = todo_title_entry.get()
+    new_todo.content = todo_content_entry.get(index1=1.0, index2="end")
+    new_todo.id = todo_id
+    
+    todo_to_be_saved = {
+        "title": new_todo.title,
+        "description": new_todo.content,
+        "id": new_todo.id
+    }
+    
+    field_is_empty = len(new_todo.title) == 0 or len(new_todo.content) == 0
+    if field_is_empty:
+        messagebox.showerror(title="EmptyFieldError!", message="Please don't leave any field empty.")
+    else:
+        whole_json_data['todo'].update(todo_to_be_saved)
+        with open("data.json", mode='w') as write_file:
+            json.dump(whole_json_data, write_file, indent=4)
+        messagebox.showinfo(title='Todo Added', message='Todo has been created.')
+        todo_title_label.destroy()
+        todo_title_entry.destroy()
+        todo_content_label.destroy()
+        todo_content_entry.destroy()
+        todo_save_button.destroy()
+        
+    
 # ------------------------- MANAGE THE FOUR TABS -------------------------- #
 
 def create_todo():
-    global todo_description_entry, todo_title_entry, todo_tab, create_todo_button
-    create_todo_button.configure(state='disabled')
-
-    Label(todo_tab, text="Title", font=FONT, padx=5, pady=5).grid(row=1, column=0)
+    global todo_content_entry, todo_title_entry, todo_tab, todo_save_button, todo_create_button, todo_content_label, todo_title_label
+    todo_create_button.configure(state='disabled')
+    
+    todo_title_label = Label(todo_tab, text="Title", font=FONT, padx=5, pady=5)
+    todo_title_label.grid(row=1, column=0)
     todo_title_entry = Entry(todo_tab, font=FONT, width=40)
+    # todo_title_entry.focus()
     todo_title_entry.grid(row=2, column=0)
     
-    Label(todo_tab, text="Description: ", font=FONT).grid(row=3, column=0)
-    todo_description_entry = Text(todo_tab, font=FONT, width=40, height=6)
-    todo_description_entry.grid(row=4, column=0)
-    save_todo_button = Button(todo_tab, text="  Save  ", font=FONT, command=save_todo)
-    save_todo_button.grid(row=5, column=0)
+    todo_content_label = Label(todo_tab, text="Description: ", font=FONT)
+    todo_content_label.grid(row=3, column=0)
+    todo_content_entry = Text(todo_tab, font=FONT, width=40, height=6)
+    todo_content_entry.grid(row=4, column=0)
+    todo_save_button = Button(todo_tab, text="  Save  ", font=FONT, command=save_todo)
+    todo_save_button.grid(row=5, column=0)
 
 
 def show_todos():
     pass
+
+
+# ---------------------------------- FILE HANDLING --------------------------------------
+
+# todo_id and sub_id handling
+try:
+    with open("id.txt") as id_file:
+        hw_id = int(id_file.read())
+except FileNotFoundError:
+    with open("id.txt", mode='w') as id_file:
+        first_hw_id = 100
+        id_file.write(f'{first_hw_id}')
+
+try:
+    with open("todo_id.txt") as id_file:
+        todo_id = int(id_file.read())
+except FileNotFoundError:
+    with open("todo_id.txt", mode='w') as id_file:
+        todo_id = 1000
+        id_file.write(f'{todo_id}')
+
+try:
+    with open('data.json', mode='r') as file:
+        whole_json_data = json.load(file)
+except FileNotFoundError:
+    with open('data.json', mode='w') as file:
+        file_structure = {
+                    "homework": {},
+                    "todo": {},
+                    "notes": {},
+                    "completed_hw": {}
+        }
+        json.dump(file_structure, file, indent=4)
 
 
 # ------------------------------ UI SETUP ----------------------------------- #
@@ -161,7 +212,7 @@ try:
         run_count = int(run_file.read())
 except FileNotFoundError:
     with open('run_counter.txt', mode='w') as run_file:
-        run_file.write("80")
+        run_file.write("1")
 else:
     with open('run_counter.txt', mode='w') as run_file:
         run_count += 1
@@ -171,26 +222,23 @@ else:
 window = Tk()
 window.title(string="Homework Aid")
 
-due_entry = Entry()
-description_entry = Entry()
-subject_entry = Entry()
-todo_description_entry = Entry()
+# global variables so that their value can be read/destroyed anywhere in this code
+hw_due_entry = Entry()
+hw_content_entry = Entry()
+hw_subject_entry = Entry()
+todo_title_label = Label()
 todo_title_entry = Entry()
-create_todo_button = Button()
+todo_content_label = Label()
+todo_content_entry = Text()
+todo_save_button = Button()
 
-try:
-    with open("id.txt") as id_file:
-        sub_id = int(id_file.read())
-except FileNotFoundError:
-    with open("id.txt", mode='w') as id_file:
-        id_file.write("100")
 
 #  uncomment below for ugly styling
 style = ttk.Style(window)
-style.theme_create("MyStyle", parent="alt", settings={
-    "TNotebook": {"configure": {"tabmargins": [20, 0, 2, 0]}},
-    "TNotebook.Tab": {"configure": {"padding": [10, 90, 10, 90]}, }})
-style.theme_use("MyStyle")
+# style.theme_create("MyStyle", parent="alt", settings={
+#     "TNotebook": {"configure": {"tabmargins": [20, 0, 2, 0]}},
+#     "TNotebook.Tab": {"configure": {"padding": [10, 90, 10, 90]}, }})
+# style.theme_use("MyStyle")
 style.configure('left_tab.TNotebook', tabposition='wn')
 
 notebook = ttk.Notebook(window, style='left_tab.TNotebook', width=1800, height=1000)
@@ -201,7 +249,8 @@ dash_tab, assignments_tab, notes_tab, todo_tab, hw_tab = all_tabs
 Label(dash_tab, text="Welcome to your HomeWork assistance system", font=FONT, pady=10).grid(row=1, column=1)
 
 # ASSIGNMENT TAB
-Button(assignments_tab, text="Create homework", font=("times", 16, "normal"), command=lambda: add_hw_button(hw_tab)).grid(row=0, column=4, sticky=W)
+Button(assignments_tab, text="Create homework", font=FONT, command=lambda: add_hw_button(hw_tab))\
+    .grid(row=0, column=4, sticky=W)
 Label(assignments_tab, text="Due Homeworks", font=("Arial", 16, "bold"), padx=10).grid(row=0, column=0)
 Label(assignments_tab, text="✔/x", padx=5, font=("Arial", 16, "bold")).grid(row=1, column=0)
 Label(assignments_tab, text="Subjects", padx=10, font=("Arial", 16, "bold")).grid(row=1, column=1)
@@ -210,9 +259,8 @@ Label(assignments_tab, text="Due Date", padx=10, font=("Arial", 16, "bold")).gri
 show_homework()
 
 Label(todo_tab, text="Anything you want to do later? Add them here.", font=FONT, pady=20).grid(row=0, column=0)
-create_todo_button = Button(todo_tab, text="Create", command=create_todo)  # hide it after clicking it
-create_todo_button.grid(row=0, column=4)
-
+todo_create_button = Button(todo_tab, text="Create", command=create_todo)  # hide it after clicking it
+todo_create_button.grid(row=0, column=4)
 show_todos()
 
 window.mainloop()
