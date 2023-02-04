6050ce6c51c87a1960b4dcc0cfc56d3d0ab1b7ab
jpt <jpt@jpt.com>
2023-02-04 10:12:30 +0545

    added signup/login sectiom

--- main.py
+++ main.py
@@ -3,11 +3,17 @@ from tkinter import *
 from tkinter import messagebox
 from tkinter import ttk
 from todo import Todo
-from tkcalendar import Calendar, DateEntry
+from tkcalendar import Calendar
+
+# import time
 
 # ------------------------------- CONSTANTS ----------------------------------- #
 
 FONT = ("Arial", 16, 'normal')
+DUE_HW_COUNTER = 0
+USERNAME = 'RAMMANI'
+PASSWORD_LEN = 4
+LOGGED_IN = False
 
 
 # ------------------------------ FILE HANDLING -------------------------------- #
@@ -20,18 +26,25 @@ def create_five_tabs(nb):
     tab3 = ttk.Frame(nb)
     tab4 = ttk.Frame(nb)
     tab5 = ttk.Frame(nb)
-    
+    tab_style = ttk.Style()
+    tab_style.configure('TNotebook.Tab',
+                        font=('Arial', 16, 'bold'),
+                        foreground='blue',
+                        background='red',
+                        width=15,
+                        padding=(5, 10, 5, 10))
     nb.add(tab1, text="    Dashboard  ")
     nb.add(tab2, text="   Assignments ")
     nb.add(tab3, text="      Notes  ")
     nb.add(tab4, text="    Todo  ")
     nb.add(tab5, text="Add Assignments", state='hidden')
-    nb.grid(row=0, column=0, sticky="")
+    nb.pack()
     nb.enable_traversal()
     return tab1, tab2, tab3, tab4, tab5
 
 
 # ---------------------------- BACKEND?? --------------------------------- #
+
 
 def insert_to_due_entry(date_picked):
     calendar.destroy()
@@ -39,28 +52,34 @@ def insert_to_due_entry(date_picked):
     date = date_picked.get()
     hw_due_entry.delete('0', END)
     hw_due_entry.insert('0', date)
-    print(date)
 
 
 def pick_due_date():
-    global hw_tab, pick_date_button, calendar, confirm_date_button
-    date_picked = StringVar(hw_tab, Calendar.date.today().strftime("%d/%m/%y"))
-    # pick_date_button['state'] = 'disabled'
-    calendar = Calendar(hw_tab, date_pattern="dd/mm/yyyy", textvariable=date_picked, showweeknumbers=False,
-                        showothermonthdays=False)
-    confirm_date_button = Button(hw_tab, text='Confirm date', command=lambda: insert_to_due_entry(date_picked))
+    global calendar, confirm_date_button
+    date_picked = StringVar(add_hw_tab, Calendar.date.today().strftime("%d/%m/%y"))
+    calendar = Calendar(
+        add_hw_tab,
+        date_pattern="dd/mm/yyyy",
+        textvariable=date_picked,
+        showweeknumbers=False,
+        showothermonthdays=False
+    )
+    confirm_date_button = Button(add_hw_tab, text='Confirm date', command=lambda: insert_to_due_entry(date_picked))
     confirm_date_button.grid(row=4, column=5)
     calendar.grid(row=4, column=3)
 
 
 def add_hw_button(tab_hw):
-    global notebook, hw_due_entry, pick_date_button, hw_subject_entry, hw_content_entry
-    notebook.tab(4, state='normal')
-    notebook.select(4)
+    global hw_due_entry, pick_date_button, hw_subject_entry, hw_content_entry
+    
+    # un-hide the hidden tab (tab number 4 is "Add Hw Tab")
+    notebook.tab(add_hw_tab, state='normal')
+    notebook.select(add_hw_tab)
     
     # 1st row
     Label(tab_hw, text="Enter Subject", font=FONT, pady=10, padx=20).grid(row=1, column=1)
     hw_subject_entry = Entry(tab_hw, width=40, font=FONT)
+    hw_subject_entry.focus()
     hw_subject_entry.grid(row=1, column=2)
     
     # 2nd row
@@ -73,42 +92,38 @@ def add_hw_button(tab_hw):
     hw_due_entry = Entry(tab_hw, width=40, font=FONT)
     hw_due_entry.grid(row=3, column=2)
     
-    # the emoji dosesnot display correctly
+    # the calendar emoji  🗓 doses not display correctly
     pick_date_button = Button(tab_hw, text="📅Pick a date 🗓️", font=FONT, command=pick_due_date)
     pick_date_button.grid(row=3, column=3)
     
-    # date_picked = StringVar(hw_tab)
-    # date_picker = DateEntry(tab_hw, date_pattern='dd/mm/yyyy', textvariable=date_picked)
-    # date = date_picked.get()
-    # hw_due_entry.delete('0', END)
-    # hw_due_entry.insert('0', date)
-    # date_picker.grid(row=3, column=3)
-    
     # save button
-    Button(tab_hw, text="Save Homework", font=FONT, command=save_hw).grid(row=5, column=2)
+    Button(tab_hw, text="Save Homework", font=FONT, command=save_hw, relief=GROOVE).grid(row=5, column=2)
 
 
-def show_homework():
-    global assignments_tab
+def refresh_homeworks():
+    global DUE_HW_COUNTER
     try:
         with open("data.json") as read_file:
-            hw = json.load(read_file)['homework']
+            hw = json.load(read_file)[f'{USERNAME}']['homework']
     except FileNotFoundError:
         messagebox.showerror(title="File not found!", message="No data file has been found!")
     else:
         row = 2
         for subject in hw:
             # display all items in HOMEWORK_LIST
-            check_button = Checkbutton(assignments_tab, text='Completed?', font=FONT)
-            check_button.grid(row=row, column=0)
-            Label(assignments_tab, text=f"{subject}", padx=10, font=FONT).grid(row=row, column=1)
-            Label(assignments_tab, text=f"{hw[subject]['description']}", padx=20, font=FONT).grid(row=row, column=2)
-            Label(assignments_tab, text=f"{hw[subject]['due']}", padx=10, font=FONT).grid(row=row, column=4)
-            row += 1
+            if hw[subject]['completed'] is False:
+                DUE_HW_COUNTER += 1
+                checked = IntVar()
+                check_button = Checkbutton(assignments_tab, text='Completed?', variable=checked, font=FONT)
+                check_button.grid(row=row, column=0)
+                Label(assignments_tab, text=f"{subject}", padx=10, font=FONT).grid(row=row, column=1)
+                Label(assignments_tab, text=f"{hw[subject]['description']}", padx=20, font=FONT).grid(row=row, column=2)
+                Label(assignments_tab, text=f"{hw[subject]['due']}", padx=10, font=FONT).grid(row=row, column=4)
+                row += 1
 
 
 def save_hw():
-    global hw_due_entry, hw_subject_entry, hw_content_entry, hw_id, whole_json_data
+    global hw_id
     hw_id += 1
     with open("id.txt", mode='w') as write_file:
         write_file.write(f"{hw_id}")
@@ -129,34 +144,33 @@ def save_hw():
     if field_is_empty:
         messagebox.showerror(title="EmptyFieldError!", message="Please don't leave any field empty.")
     else:
-        whole_json_data['homework'].update(new_homework)
+        current_user_data['homework'].update(new_homework)
         with open("data.json", mode='w') as write_file:
-            json.dump(whole_json_data, write_file, indent=4)
-        
+            json.dump(complete_data, write_file, indent=4)
         messagebox.showinfo(title='Homework Added', message="Homework has been added! ")
-        messagebox.Message(notebook)
+        
         hw_due_entry.delete(0, END)
         hw_content_entry.delete(0, END)
         hw_subject_entry.delete(0, END)
         
-        notebook.tab(4, state='hidden')
-        notebook.select(1)
-        show_homework()
+        notebook.tab(add_hw_tab, state='hidden')
+        notebook.select(assignments_tab)
+        refresh_homeworks()
 
 
 def save_todo():
-    global todo_content_entry, todo_title_entry, todo_tab, todo_create_button, todo_content_label, todo_title_label, todo_id, whole_json_data
-    todo_id += 1
+    global LATEST_TODO_ID
+    LATEST_TODO_ID += 1
     todo_create_button.configure(state='normal')
     new_todo = Todo()
     todo_create_button.configure(state='normal')
     
     with open("todo_id.txt", mode='w') as todo_id_file:
-        new_todo.id = todo_id_file.write(f'{todo_id}')
+        new_todo.id = todo_id_file.write(f'{LATEST_TODO_ID}')
     
     new_todo.title = todo_title_entry.get()
     new_todo.content = todo_content_entry.get(index1=1.0, index2="end")
-    new_todo.id = todo_id
+    new_todo.id = LATEST_TODO_ID
     
     todo_to_be_saved = {
         f"{new_todo.title}": {
@@ -169,9 +183,9 @@ def save_todo():
     if field_is_empty:
         messagebox.showerror(title="EmptyFieldError!", message="Please don't leave any field empty.")
     else:
-        whole_json_data['todo'].update(todo_to_be_saved)
+        current_user_data['todo'].update(todo_to_be_saved)
         with open("data.json", mode='w') as write_file:
-            json.dump(whole_json_data, write_file, indent=4)
+            json.dump(complete_data, write_file, indent=4)
         messagebox.showinfo(title='Todo Added', message='Todo has been created.')
         todo_title_label.destroy()
         todo_title_entry.destroy()
@@ -183,9 +197,9 @@ def save_todo():
 # ------------------------- MANAGE THE FOUR TABS -------------------------- #
 
 def create_todo():
-    global todo_content_entry, todo_title_entry, todo_tab, todo_save_button, todo_create_button, todo_content_label, todo_title_label
-    todo_create_button.configure(state='disabled')
+    global todo_content_entry, todo_title_entry, todo_save_button, todo_content_label, todo_title_label
     
+    todo_create_button.configure(state='disabled')
     todo_title_label = Label(todo_tab, text="Title", font=FONT, padx=5, pady=5)
     todo_title_label.grid(row=1, column=0)
     todo_title_entry = Entry(todo_tab, font=FONT, width=40)
@@ -204,7 +218,93 @@ def show_todos():
     pass
 
 
-# ---------------------------------- FILE HANDLING --------------------------------------
+# --------------------------------- LOGIN MANAGEMENT ------------------------------------#
+
+
+def signup(root, username, password, confirm_password):
+    global USERNAME
+    root.destroy()
+    if username.get() in login_data:
+        messagebox.showerror(title='Username Error!', message='This username has been taken. Please chose another one.')
+        signup_screen()
+    elif len(password.get()) < PASSWORD_LEN:
+        messagebox.showerror(title='Too short password!', message='Password must be at least 8 characters')
+        signup_screen()
+    elif password.get() != confirm_password.get():
+        messagebox.showerror(title='Password did not Match',
+                             message='confirmation of new password failed, they did not match!  ')
+        signup_screen()
+    else:
+        messagebox.showinfo(title='Success', message=f'Your account has been created\n username:{username.get()}\npassword: {password.get()}')
+        new_user = {
+            f"{username.get()}": {
+                "password": f"{password.get()}"
+            }
+        }
+        login_data.update(new_user)
+        with open('login.json', mode='w') as write_file:
+            json.dump(login_data, write_file, indent=4)
+            
+            
+def signup_screen():
+    global username_label, username_entry, password_entry, password_label
+    signup_window = Tk()
+    signup_window.title = 'signup screen'
+
+    username = StringVar()
+    password = StringVar()
+    confirm_password = StringVar()
+    
+    username_label = Label(signup_window, text='Username', font=FONT)
+    username_label.pack()
+    
+    username_entry = Entry(signup_window, font=FONT, textvariable=username)
+    username_entry.pack()
+    username_entry.focus()
+    
+    password_label = Label(signup_window, text='Password(minimum 8 characters)', font=FONT)
+    password_label.pack()
+    
+    password_entry = Entry(signup_window, font=FONT, textvariable=password)
+    password_entry.pack()
+    
+    Label(signup_window, font=FONT, text='confirm password').pack()
+    confirm_password_entry = Entry(signup_window, font=FONT, textvariable=confirm_password)
+    confirm_password_entry.pack()
+    
+    signup_button = Button(signup_window, text="Signup", command=lambda: signup(signup_window, username, password, confirm_password))
+    signup_button.pack()
+    
+    signup_window.mainloop()
+    
+
+def login(root):
+    username = username_entry.get()
+    password = password_entry.get()
+
+
+def login_screen():
+    global username_label, username_entry, password_entry, password_label
+    
+    root = Tk()
+    Label(text="Login to use the app\n(Create one if you don't have an account)").pack()
+    username_label = Label(root, text='Username', font=FONT)
+    username_label.pack()
+    username_entry = Entry(root, font=FONT)
+    username_entry.pack()
+    username_entry.focus()
+    password_label = Label(root, text='Password(minimum 8 characters)', font=FONT)
+    password_label.pack()
+    password_entry = Entry(root, font=FONT)
+    password_entry.pack()
+    login_button = Button(root, text="login", command=lambda: login(root))
+    login_button.pack()
+    
+    root.mainloop()
+
+
+# ---------------------------------- FILE HANDLING -------------------------------------- #
+
 
 # todo_id and sub_id handling
 try:
@@ -217,28 +317,32 @@ except FileNotFoundError:
 
 try:
     with open("todo_id.txt") as id_file:
-        todo_id = int(id_file.read())
+        LATEST_TODO_ID = int(id_file.read())
 except FileNotFoundError:
     with open("todo_id.txt", mode='w') as id_file:
-        todo_id = 1000
-        id_file.write(f'{todo_id}')
+        LATEST_TODO_ID = 1000
+        id_file.write(f'{LATEST_TODO_ID}')
 
 try:
     with open('data.json', mode='r') as file:
-        whole_json_data = json.load(file)
+        complete_data = json.load(file)
+        current_user_data = complete_data[f'{USERNAME}']
 except FileNotFoundError:
     with open('data.json', mode='w') as file:
         file_structure = {
-            "homework": {},
-            "todo": {},
-            "notes": {},
-            "completed_hw": {}
+            f"{USERNAME}": {
+                "homework": {},
+                "todo": {},
+                "notes": {},
+                "completed_hw": {}
+            }
         }
         json.dump(file_structure, file, indent=4)
 
-# ------------------------------ UI SETUP ----------------------------------- #
-
-# delete this block on completion of code
+'''
+delete this block (below) on completion of code
+this block of code is set to count how many times the code has been run
+'''
 try:
     with open('run_counter.txt') as run_file:
         run_count = int(run_file.read())
@@ -251,49 +355,78 @@ else:
         run_file.write(f"{run_count}")
         print(run_count)
 
-window = Tk()
-window.title(string="Homework Aid")
+""" Login file below """
+try:
+    with open('login.json') as login_file:
+        # login_data = json.load(login_file)  # use this line later
+        login_data = {
+            'default': {
+                "password": "sudo"
+            }
+        }
+except FileNotFoundError:
+    messagebox.showinfo(title='Login or Signup to continue!',
+                        message="You must be logged in to use the service.\nOr create a new account")
+    login_data = {
+        'default': {
+            "password": "sudo"
+        }
+    }
+    signup_screen()
+# ---------------------------------- PROGRAM STARTS HERE ------------------------------------- #
 
-# global variables so that their value can be read/destroyed anywhere in this code
-hw_due_entry = Entry()
-hw_content_entry = Entry()
-hw_subject_entry = Entry()
-todo_title_label = Label()
-todo_title_entry = Entry()
-todo_content_label = Label()
-todo_content_entry = Text()
-todo_save_button = Button()
-pick_date_button = Button()
-calendar = Calendar()
-confirm_date_button = Button()
-#  uncomment below for ugly styling
-style = ttk.Style(window)
-# style.theme_create("MyStyle", parent="alt", settings={
-#     "TNotebook": {"configure": {"tabmargins": [20, 0, 2, 0]}},
-#     "TNotebook.Tab": {"configure": {"padding": [10, 90, 10, 90]}, }})
-# style.theme_use("MyStyle")
-style.configure('left_tab.TNotebook', tabposition='wn')
+login_screen()
 
-notebook = ttk.Notebook(window, style='left_tab.TNotebook', width=1800, height=1000)
-all_tabs = create_five_tabs(notebook)
-dash_tab, assignments_tab, notes_tab, todo_tab, hw_tab = all_tabs
-
-# DASH TAB
-Label(dash_tab, text="Welcome to your HomeWork assistance system", font=FONT, pady=10).grid(row=1, column=1)
-
-# ASSIGNMENT TAB
-Button(assignments_tab, text="Create homework", font=FONT, command=lambda: add_hw_button(hw_tab)) \
-    .grid(row=0, column=4, sticky=W)
-Label(assignments_tab, text="Due Homeworks", font=("Arial", 16, "bold"), padx=10).grid(row=0, column=0)
-Label(assignments_tab, text="✔/x", padx=5, font=("Arial", 16, "bold")).grid(row=1, column=0)
-Label(assignments_tab, text="Subjects", padx=10, font=("Arial", 16, "bold")).grid(row=1, column=1)
-Label(assignments_tab, text="Description", padx=20, font=("Arial", 16, "bold")).grid(row=1, column=2, columnspan=2)
-Label(assignments_tab, text="Due Date", padx=10, font=("Arial", 16, "bold")).grid(row=1, column=4)
-show_homework()
-
-Label(todo_tab, text="Anything you want to do later? Add them here.", font=FONT, pady=20).grid(row=0, column=0)
-todo_create_button = Button(todo_tab, text="Create", command=create_todo)  # hide it after clicking it
-todo_create_button.grid(row=0, column=4)
-show_todos()
-
-window.mainloop()
+if LOGGED_IN:
+    window = Tk()
+    window.title(string="Homework Aid")
+    
+    # global WIDGETS so that they can be READ/DESTROYED anywhere in the code
+    hw_due_entry = Entry()
+    hw_content_entry = Entry()
+    hw_subject_entry = Entry()
+    todo_title_label = Label()
+    todo_title_entry = Entry()
+    todo_content_label = Label()
+    todo_content_entry = Text()
+    todo_save_button = Button()
+    pick_date_button = Button()
+    calendar = Calendar()
+    confirm_date_button = Button()
+    
+    username_label = Label()
+    username_entry = Entry()
+    password_label = Label()
+    password_entry = Entry()
+    
+    #  uncomment below for ugly styling
+    style = ttk.Style(window)
+    # style.theme_create("MyStyle", parent="alt", settings={
+    #     "TNotebook": {"configure": {"tabmargins": [20, 0, 2, 0]}},
+    #     "TNotebook.Tab": {"configure": {"padding": [10, 90, 10, 90]}, }})
+    # style.theme_use("MyStyle")
+    style.configure('left_tab.TNotebook', tabposition='wn')
+    
+    notebook = ttk.Notebook(window, style='left_tab.TNotebook')
+    all_tabs = create_five_tabs(notebook)
+    dash_tab, assignments_tab, notes_tab, todo_tab, add_hw_tab = all_tabs
+    
+    # DASH TAB
+    Label(dash_tab, text="Welcome to your HomeWork assistance system", font=FONT, pady=10).grid(row=1, column=1)
+    
+    # ASSIGNMENT TAB
+    Button(assignments_tab,
+           text="Create homework", font=FONT, command=lambda: add_hw_button(add_hw_tab)).grid(row=0, column=4)
+    Label(assignments_tab, text="Due Homeworks", font=("Arial", 16, "bold"), padx=10).grid(row=0, column=0)
+    Label(assignments_tab, text=" Status ", padx=5, font=("Arial", 16, "bold")).grid(row=1, column=0)
+    Label(assignments_tab, text="Subjects", padx=10, font=("Arial", 16, "bold")).grid(row=1, column=1)
+    Label(assignments_tab, text="Description", padx=20, font=("Arial", 16, "bold")).grid(row=1, column=2, columnspan=2)
+    Label(assignments_tab, text="Due Date", padx=10, font=("Arial", 16, "bold")).grid(row=1, column=4)
+    refresh_homeworks()
+    
+    Label(todo_tab, text="Anything you want to do later? Add them here.", font=FONT, pady=20).grid(row=0, column=0)
+    todo_create_button = Button(todo_tab, text="Create", command=create_todo)  # hide it after clicking it
+    todo_create_button.grid(row=0, column=4)
+    show_todos()
+    
+    window.mainloop()
